baf940396abe7aa9f67c36aac30e6d638190626c
AlexsanderCDambros <alexsanderdambros@gmail.com>
2020-05-28 19:26:03 -0300

    Aula 8 e Aula 8 Exercícios

---
+++ ComparacaoString/src/comparacaostring/ComparacaoString.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comparacaostring;
+
+/**
+ *
+ * @author Alexsander Chagas Dambros
+ */
+public class ComparacaoString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nome1 = "Alex";
+        String nome2 = "Alex";
+        String nome3 = new String("Alex");
+        String res;
+        
+        // Tipos Primitivos
+        res = nome1==nome2?"Igual":"Diferente";
+        System.out.println(res);
+        
+        // Classes Invólucro
+        res = nome1.equals(nome3)?"Igual":"Diferente";
+        System.out.println(res);
+    }
+    
+}
---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,17 @@
+package operadorternario;
+
+/**
+ *
+ * @author Alexsander Chagas Dambros
+ */
+public class OperadorTernario {
+    
+    public static void main(String[] args) {
+        int n1, n2, r;
+        n1 = 14;
+        n2 = 20;
+        r = n1>n2?n1+n2:n1-n2;
+        System.out.println(r);
+    }
+    
+}
